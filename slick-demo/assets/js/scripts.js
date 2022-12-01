1ac5598256e5c77393053e53eee28d7c2530285e
zmatlock <zmatlock@ualr.edu>
2022-12-01 16:05:46 +0000

    added slideshow

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 400,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
